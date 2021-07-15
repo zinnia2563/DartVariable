0f825b0233838e483aa56d4fe4f74fc87a45bced
Your Name <you@example.com>
2021-07-15 06:01:16 +0600

    variables and concatenation

---
+++ DartVariable.dart
@@ -0,0 +1,7 @@
+main(){
+    String first_name ="Nazma";//type safety , var name = "Nazma" is non type safety
+    String middle_name="Akter";
+    String last_name="Zinnia";
+    
+    print(first_name+" "+middle_name+" "+last_name);//concatenation
+}
